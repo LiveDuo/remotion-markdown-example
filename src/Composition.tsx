7254aa2eb7a30c069e2ea6634bbf0b60a464f12a
Andreas Tzionis <andy0n7@gmail.com>
2023-06-26 09:44:59 +0300

    added duration, cleanup

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,12 +1,22 @@
+import { CSSProperties } from 'react'
+
 import { AbsoluteFill, interpolate, Sequence, useCurrentFrame } from 'remotion'
 
 import ReactMarkdown from 'react-markdown'
 
-const slides = ['# Slide 1 \n Hello world ', '# Slide 2']
+interface Slice { content: string; duration?: number; }
+
+const DURATION_DEFAULT = 50
+
+const slides: Slice[] = [{content: '# Slide 1 \n Hello world ', duration: 80}, {content: '# Slide 2'}]
+
+const containerOptions: CSSProperties = {position: 'relative', margin: '40px', fontSize: '3em', justifyContent: 'center', alignItems: 'center'}
+
+const slideFrom = (i: number) => slides.reduce((s, c, j) => (i > j) ? s + (c.duration! ?? DURATION_DEFAULT) : s, 0) ?? 0
 
 const Slide: React.FC<{text: string}> = ({text}) => {
 	const frame = useCurrentFrame()
-	const opacity = interpolate(frame, [0, 10], [0, 1])
+	const opacity = interpolate(frame, [0, 30], [0, 1])
 	return (
 		<div style={{opacity}}>
 			<ReactMarkdown>{text}</ReactMarkdown>
@@ -17,10 +27,14 @@ const Slide: React.FC<{text: string}> = ({text}) => {
 const Component: React.FC = () => {
 	return (
 		<AbsoluteFill style={{backgroundColor: 'white'}}>
-			{slides.map((text: string, i: number) => 
-				<Sequence from={i * 50} durationInFrames={50} style={{position: 'relative', margin: '40px', fontSize: '3em'}}>
-					<Slide text={text}/>
-				</Sequence>
+			{slides.map((slide: Slice, i: number) => {
+				const duration = slide.duration ?? DURATION_DEFAULT
+				return (
+					<Sequence key={i} from={slideFrom(i)} durationInFrames={duration} style={containerOptions}>
+						<Slide text={slide.content}/>
+					</Sequence>
+					)
+				}
 			)}
 		</AbsoluteFill>
 	)
